188b48c28ff6d94ab55c2b6a6e12ba02dc3d7350
mauavilespi <mauavilespi@outlook.com>
2019-10-08 14:51:47 -0500

    Agregando la solución al problema 10055 de UVa Online Judge

---
+++ UVaOnlineJudge/10055/hashmat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (){
+
+    long long a, b;
+
+    while (cin>>a>>b){
+        cout << abs(a-b)<<'\n';
+    } 
+    return 0;
+
+}
