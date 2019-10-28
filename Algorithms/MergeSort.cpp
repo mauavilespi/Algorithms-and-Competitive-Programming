94be5b147fceb139295f4c269ca28c37afb14070
Mauricio Aviles Piña <mauavilespi@outlook.com>
2019-10-28 11:09:53 -0600

    Create MergeSort.cpp

---
+++ Algorithms/MergeSort.cpp
@@ -0,0 +1,39 @@
+//IMPLEMENTACIÓN MERGESORT
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeSort(vector <int> con, int l, int m, int r);
+
+
+int main(){
+    return 0;
+}
+
+void mergeSort(vector <int> con, int l, int m, int r){
+        
+    int i, j, k;
+    
+    int n1=m-l;
+    int n2=r-m-1;
+    
+    vector <int> L [n1];
+    vector <int> R [n2];
+    
+    for (i=0; i<=n1; i++)
+        L[i]=con[i];
+    for (j=0; j<=n2; j++)
+        R[j]=con[m+1+j]
+    
+    
+    i=0;
+    j=0;
+    k=0;
+    
+    
+    
+    
+    
+    
+}
